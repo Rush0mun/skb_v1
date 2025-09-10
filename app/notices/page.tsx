d41869cde9e9ac586d977066ca528b2efff023c8
Rush0mun <rushnanmuntaka@iut-dhaka.edu>
2025-09-10 22:16:09 +0600

    imports updated

--- app/notices/page.tsx
+++ app/notices/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Bell, Filter } from 'lucide-react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import NoticeCard from '@/components/notices/NoticeCard';
